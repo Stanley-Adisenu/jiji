10b363ca886ec726a1e1a51999734d69befe8428
stanleyadisenu@gmail.com <stanleyadisenu@gmail.com>
2024-06-11 17:52:36 +0000

    ujgjhv,j

--- jijiapp/settings.py
+++ jijiapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import  dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,15 +79,19 @@ WSGI_APPLICATION = 'jijiapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'miniProject',
-        'USER': 'postgres',
-        'PASSWORD': '@Gravity1',
-        'HOST': 'localhost', 
-        'PORT': 5432, 
-    }
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'miniProject',
+#         'USER': 'postgres',
+#         'PASSWORD': '@Gravity1',
+#         'HOST': 'localhost', 
+#         'PORT': 5432, 
+#     }
+# }
+
+DATABASES={
+    'default':dj_database_url.parse("postgres://jiji_database_user:QEz6AqdE1vliJjupW5cEXYcv6jB7NnTW@dpg-cpk8cvsf7o1s73ckkk4g-a.oregon-postgres.render.com/jiji_database")
 }
 
 
